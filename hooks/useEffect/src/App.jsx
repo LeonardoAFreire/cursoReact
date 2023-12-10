1d5f3f7c2aa41fa1669090b494e75761e4bda916
Leonardo Freire <leoaguiarfreire@gmail.com>
2023-12-09 23:59:31 -0300

    exercicio useEffect

--- hooks/useEffect/src/App.jsx
+++ hooks/useEffect/src/App.jsx
@@ -4,9 +4,38 @@
 // Mostre o nome e preço na tela (separe essa informação em um componente Produto.js)
 // Defina o produto clicado como uma preferência do usuário no localStorage
 // Quando o usuário entrar no site, se existe um produto no localStorage, faça o fetch do mesmo
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Produto from './Produto';
 
 function App() {
-  return <></>;
+  const [produto, setProduto] = useState(null);
+
+  function handleClick({ target }) {
+    setProduto(target.innerText);
+  }
+
+  useEffect(() => {
+    const temProduto = localStorage.getItem('produto');
+    if (temProduto) {
+      setProduto(temProduto);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (produto) {
+      localStorage.setItem('produto', produto);
+    }
+  }, [produto]);
+
+  return (
+    <>
+      <h1>Preferência: {produto}</h1>
+      <button onClick={handleClick}>notebook</button>
+      <button onClick={handleClick}>smartphone</button>
+      {<Produto produto={produto} />}
+    </>
+  );
 }
 
 export default App;
--- hooks/useEffect/src/Produto.jsx
+++ hooks/useEffect/src/Produto.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
-function Produto() {
-  return <div>Produto</div>;
+function Produto({ produto }) {
+  const [dados, setDados] = useState(null);
+
+  useEffect(() => {
+    async function fetchProduto() {
+      const response = await fetch(
+        `https://ranekapi.origamid.dev/json/api/produto/${produto}`
+      );
+      const json = await response.json();
+
+      setDados(json);
+    }
+
+    if (produto) {
+      fetchProduto();
+    }
+  }, [produto]);
+
+  if (!dados) {
+    return null;
+  }
+
+  return (
+    <div>
+      <h1>{dados.nome}</h1>
+      <p>R$ {dados.preco}</p>
+      <img src={dados.fotos[0].src} alt={dados.descricao} />
+    </div>
+  );
 }
 
 export default Produto;
